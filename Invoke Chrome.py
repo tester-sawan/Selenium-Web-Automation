6bd67192a95bd1af10e4b0c6255533139599e044
tester-sawan <sawankumar.qa@gmail.com>
2025-12-07 22:06:22 +0530

    Invoking browser

---
+++ Invoke Chrome.py
@@ -0,0 +1,16 @@
+import time
+#Import for Selenium Webdriver
+from selenium import webdriver
+#Invoke Chrome Browser
+driver = webdriver.Chrome()
+# Invoking firefox
+# driver = webdriver.Firefox()
+# Opening any url
+driver.get("https://rahulshettyacademy.com/")
+# For maximizing the window
+driver.maximize_window()
+# Know the title of the page
+print(f"The current page title is : {driver.title}")
+# Know the current page url:
+print(f"The current url is: {driver.current_url}")
+#time.sleep(2)
