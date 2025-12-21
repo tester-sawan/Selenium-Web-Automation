20ad891cd76f7c013e7e3e30c4d56799ad85e10a
Sawan Kumar <sawankumar.qa@gmail.com>
2025-12-21 21:18:19 +0530

    framework-1

--- framework/test_topic1.py
+++ framework/test_topic1.py
@@ -1,5 +1,7 @@
 # All python file should start with the 'test_' for framework framework
 # All code should be inside method only.
 # All method name should starts with 'test'
-def test_topic1():
+def test_topicFirst():
     print("Hello, Welcome to python testing framework learning.")
+def test_case():
+    print("First step in way of Python-SDET")
---
+++ framework/test_topic2.py
@@ -0,0 +1,3 @@
+def test_topicFirst():
+    msg = "Access Granted"
+    assert msg == "Access Granted","Not matched with the expected result."
